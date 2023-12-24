afba30b7d5638a413cdb1dabed6ed062385d43d0
nhatminhtranchuyenly <nhatminhtranchuyenly@gmail.com>
2023-12-24 21:27:01 +0700

    first commit

---
+++ config.js
@@ -0,0 +1,47 @@
+// 霸都丶傲天 2019年10月10日 https://github.com/AJLoveChina/birthday
+var config = {
+    // 句子的长度可以任意， 你可以写十句话， 二十句话都可以
+    // 每句话尽量不要超过15个字,不然展示效果可能不太好
+    texts: [
+        "   ",
+        "dear Hoài",      //这里,每句话结尾的最后一个逗号必须是英文的哦!! 很重要哦!!
+        "à ờmmm ...",  // 同上...
+        "Nay là ngày lễ giáng sinh",
+        "Ông già Noel sẽ đi phát quà cho những đứa bé ngoan",
+        "Ông bảo là do ông bị ốm nên khôn qua nhà cậu được",
+        "Nên nhờ tớ gửi món quà này cho cậu",
+        "Mong cậu có một Giáng Sinh vui vẻ bên người thân",
+        "Nhận được những món quà ý thật nghĩa",
+        "Tuy năm nay không đón giáng sinh cùng với cậu được",
+        "Chắc do năm nay tớ là đứa trẻ hư, không làm cậu cười nhiều",
+        "Những mà tớ hứa sẽ là đứa trẻ ngoan ",
+        "Để được đón Giáng Sinh với cậu vào năm sau",
+        "Thôi tớ phải đi giúp ông già Noel phát quà rôi",
+        "Pai Paiiiiiii "
+    ],
+    /**
+     * imgs 可以不填, 但是如果要填写的话必须遵循下面的格式
+     * "对应上面的文字, 要完全一样" : "图片地址, 可以把图片放在imgs文件夹中"
+     * 例如
+     * "心爱的小可爱": "./imgs/xiaokeai.jpg"
+     *
+     * 如果不要图片的话, 直接在每行开头写两个斜杠注释即可, 例如下面的 "今天是你的生日" 的图片就不会展示了:)
+     * Tip: 图片最好用正方形or接近正方形, 看起来效果更好
+     */
+    imgs: {
+        "dear Hoài" : "./imgs/409597776_7139764229415770_4127579273261064668_n.png",
+        "Để được đón Giáng Sinh với cậu vào năm sau" : "./imgs/smile.png",
+        // "今天是你的生日": "./imgs/birthday.jpg",
+    },
+    // 按钮文字描述, 以下是默认的按钮文字，英文的，您可以改成你喜欢的文字
+    desc: {
+        turn_on: "Hello Hoài :33",
+        play: "Cho Hoài nì",
+        bannar_coming: "Trang trí một chút nhá",
+        balloons_flying: "Thêm chút bóng bay",
+        cake_fadein: "Thêm 1 chiếc bánh kém siu to khổng lồ ?",
+        light_candle: "Thắp nến cho Hoài ước nè",
+        wish_message: "Merry Christmas ⛄⛄",
+        story: "À mà đúng rồi",
+    }
+};
